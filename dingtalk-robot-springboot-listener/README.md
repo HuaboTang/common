23af71768009695747115031d6fd3c3d1e8bb191
vbobot <tanghuabo1989@163.com>
2019-01-24 14:13:54 +0800

    add: add module dingtalk-robot-springboot-listener

---
+++ dingtalk-robot-springboot-listener/README.md
@@ -0,0 +1,4 @@
+# 使用
+```sh
+java -jar xxx.jar --dingtalk-webhook={钉钉机器人webhook} --app-name=应用名称
+```
---
+++ dingtalk-robot-springboot-listener/src/main/java/com/codrim/common/dingtalk/springboot/listener/DingTalkMachineAutoConfiguration.java
@@ -0,0 +1,8 @@
+package com.codrim.common.dingtalk.springboot.listener;
+
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class DingTalkMachineAutoConfiguration {
+}
