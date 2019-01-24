4fa2f21bddd1d5562fe190c593b8d56f54749a76
vbobot <tanghuabo1989@163.com>
2019-01-24 14:59:28 +0800

    change: 将dingTalkRobotListener的参数以-D的形式替换原有--的形式

--- dingtalk-robot-springboot-listener/src/main/java/com/codrim/common/dingtalk/springboot/listener/DingTalkSpringBootStartListener.java
+++ dingtalk-robot-springboot-listener/src/main/java/com/codrim/common/dingtalk/springboot/listener/DingTalkSpringBootStartListener.java
@@ -1,8 +1,8 @@
 package com.codrim.common.dingtalk.springboot.listener;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.context.event.ApplicationStartingEvent;
 import org.springframework.context.ApplicationListener;
-import org.springframework.core.env.SimpleCommandLinePropertySource;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -20,24 +20,22 @@ public class DingTalkSpringBootStartListener implements ApplicationListener<Appl
     @Override
     public void onApplicationEvent(ApplicationStartingEvent event) {
 
-        SimpleCommandLinePropertySource commandLinePropertySource = new SimpleCommandLinePropertySource(event.getArgs());
-
-        if (commandLinePropertySource.containsProperty("dingtalk-webhook")) {
-
-            final String name = MessageUtils.getServerName(commandLinePropertySource.getProperty("app-name"));
-
-            //dingtalk-webhook  ding-msg
-            Map<String, Object> map = of(
-                    "msgtype", "text",
-                    "text", of("content", "【[出差]】" + name + " starting. \n now：" +
-                            LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
-            );
-
-            String url = commandLinePropertySource.getProperty("dingtalk-webhook");
-
-            DingTalkRobotUtils.asyncSendMessage(url, map);
-        } else {
+        final String url = PropertiesGetter.getProperty("dingtalk-webhook");
+        if (StringUtils.isBlank(url)) {
             log.info("DingTalk-robot-springboot-listener, not config --dingtalk-webhook and --app-name, no send dingTalk message");
+            return;
         }
+
+        final String name = MessageUtils.getServerName(PropertiesGetter.getProperty("app-name"));
+
+        Map<String, Object> map = of(
+                "msgtype", "text",
+                "text", of("content", "【[出差]】" + name + " starting. \n now：" +
+                        LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
+        );
+
+
+        DingTalkRobotUtils.asyncSendMessage(url, map);
+
     }
 }
